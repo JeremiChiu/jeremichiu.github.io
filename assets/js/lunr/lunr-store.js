cc1e6b045f29901abd6bb833894f9af7f016b3bf
JeremiChiu <jeremichiu@gmail.com>
2018-02-10 15:29:05 -0800

    .

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,20 +2,15 @@ var store = [{
         "title": "First Post",
         "excerpt":"testing","categories": [],
         "tags": [],
-        "url": "https://jeremi.kchiu.ca/First-Post/",
+        "url": "http://localhost:4000/First-Post/",
         "teaser":null},{
         "title": "Car Detailing 2004 Toyota Echo",
         "excerpt":"This is one of the first car detailing jobs I ever did when I started to get more interested in car detailing in 2013/2014.I did this as a favour for a friend. I didn’t take too many pictures since the point of this exercise was to get my detailing chops...","categories": ["Personal_Projects"],
         "tags": ["detailing","wax","wash","paint","drive","protect"],
-        "url": "https://jeremi.kchiu.ca/personal_projects/Auto-Detailing/",
+        "url": "http://localhost:4000/personal_projects/Auto-Detailing/",
         "teaser":null},{
         "title": "Austrian-Viennese Apfelstrudel",
         "excerpt":"After visiting the Tyrol (in German: Tirol) state of Austria for a snowboarding trip I fell in love with hot “apfelstudel mit vanillesoße” (in English: Apple Strudel with Vanilla sauce) after a good day out on the slopes. The vanilla sauce is a very creamy vanilla egg custard and tastes...","categories": ["Personal_Projects"],
         "tags": ["apples","apfelstrudel","cooking","recipes"],
-        "url": "https://jeremi.kchiu.ca/personal_projects/Apfelstrudel/",
-        "teaser":null},{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "https://jeremi.kchiu.ca/jekyll/update/welcome-to-jekyll/",
+        "url": "http://localhost:4000/personal_projects/Apfelstrudel/",
         "teaser":null}]
